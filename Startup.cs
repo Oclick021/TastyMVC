548188bcfb23b8eb2983e29a345f93ce3240d606
Saeed Asgari <oclick021@gmail.com>
2020-02-08 12:52:03 +0100

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TastyMVC.Startup))]
+namespace TastyMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
